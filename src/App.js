38b3635e201c8f03b44528d31341444b6f14a9f3
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2021-04-09 22:25:15 -0300

    front_end creation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,78 @@
+import React, {Component} from 'react';
 import './App.css';
+import Navigation from './components/Navigation/navigation';
+import Logo from './components/Logo/logo';
+import ImageLinkForm from './components/ImageLingForm/imagelinkform';
+import Rank from './components/Rank/rank';
+import Particles from 'react-particles-js';
+
+const particle_options = {
+  "particles": {
+    "links": {
+      "color": {
+        "value": "#ffffff"
+      },
+      "distance": 100,
+      "opacity": 0.4,
+      "width": 1
+    },
+    "fullScreen": {
+      zIndex:2
+    },
+    "move": {
+      "attract": {
+        "rotate": {
+          "x": 600,
+          "y": 100
+        }
+      },
+      "direction": "bottom",
+      "enable": true,
+      "outModes": {
+        "bottom": "out",
+        "left": "out",
+        "right": "out",
+        "top": "out"
+      }
+    },
+    "opacity": {
+      "random": {
+        "enable": true
+      },
+      "value": {
+        "min": 0.1,
+        "max": 0.2
+      },
+      "animation": {
+        "speed": 0.5,
+        "minimumValue": 0.1
+      }
+    },
+    "size": {
+      "random": {
+        "enable": true
+      },
+      "value": {
+        "min": 1,
+        "max": 10
+      },
+      "animation": {
+        "speed": 20,
+        "minimumValue": 0.1
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <p>
-          Aqui está meu app.
-        </p>
-
-      </header>
+      <Particles className='particles' params={particle_options} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition/> */}
     </div>
   );
 }
---
+++ src/components/ImageLingForm/imagelinkform.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './imagelinkform.css';
+
+const ImageLinkForm = () =>{
+  return(
+    <div>
+      <p className='f3'>
+        {'This magic Brain will detect faces in your pictures. Give it a try.'}
+      </p>
+      <div className='center'>
+        <div className=' center pa4 br3 shadow-5 cool_bg'>
+          <input className='f4 pa2 w-70 center' type='tex'/>
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ImageLinkForm;
